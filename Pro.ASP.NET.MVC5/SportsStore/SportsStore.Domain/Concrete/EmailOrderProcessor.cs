71dfa56b13fe81cea9268aeb0f4246445cc2741e
Philip.SeokJoon.Yun <seokjoon.yun@gmail.com>
2016-04-28 08:28:44 +0900

    aa

---
+++ Pro.ASP.NET.MVC5/SportsStore/SportsStore.Domain/Concrete/EmailOrderProcessor.cs
@@ -0,0 +1,92 @@
+﻿using SportsStore.Domain.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SportsStore.Domain.Entities;
+using System.Net.Mail;
+using System.Net;
+
+namespace SportsStore.Domain.Concrete
+{
+    public class EmailSessings
+    {
+        public string MailToAddress = "orders@example.com";
+        public string MailFromAddress = "sportsstore@examlpe.com";
+        public bool UseSsl = true;
+        public string Username = "MySmtpUsername";
+        public string Password = "MySmtpPassword";
+        public string ServerName = "smtp.example.com";
+        public int ServerPort = 587;
+        public bool WriteAsFile = false;
+        public string FileLocation = @"c:\sports_sotre_emails";
+    }
+
+    public class EmailOrderProcessor : IOrderProcessor
+    {
+        private EmailSessings emailSettings;
+
+        public EmailOrderProcessor(EmailSessings settings)
+        {
+            emailSettings = settings;
+        }
+        public void ProcessOrder(Cart cart, ShippingDetails shippingInfo)
+        {
+            using (var smtpClient = new SmtpClient())
+            {
+                smtpClient.EnableSsl = emailSettings.UseSsl;
+                smtpClient.Host = emailSettings.ServerName;
+                smtpClient.Port = emailSettings.ServerPort;
+                smtpClient.UseDefaultCredentials = false;
+                smtpClient.Credentials = new NetworkCredential(emailSettings.Username, emailSettings.Password);
+
+                if (emailSettings.WriteAsFile)
+                {
+                    smtpClient.DeliveryMethod = SmtpDeliveryMethod.SpecifiedPickupDirectory;
+                    smtpClient.PickupDirectoryLocation = emailSettings.FileLocation;
+                    smtpClient.EnableSsl = false;
+                }
+
+                StringBuilder body = new StringBuilder()
+                    .AppendLine("A new order has been submitted")
+                    .AppendLine("---")
+                    .AppendLine("Items:");
+
+                foreach (var line in cart.Lines)
+                {
+                    var subtotal = line.Product.Price * line.Quantity;
+                    body.AppendLine($"{line.Quantity} x {line.Product.Name} (subtotal: {subtotal}");
+                }
+
+
+                body.AppendLine($"Total order value: {cart.ComputeTotalValue()}")
+                    .AppendLine("---")
+                    .AppendLine("Ship to:")
+                    .AppendLine(shippingInfo.Name)
+                    .AppendLine(shippingInfo.Line1)
+                    .AppendLine(shippingInfo.Line2 ?? "")
+                    .AppendLine(shippingInfo.Line3 ?? "")
+                    .AppendLine(shippingInfo.City)
+                    .AppendLine(shippingInfo.State ?? "")
+                    .AppendLine(shippingInfo.Country)
+                    .AppendLine(shippingInfo.Zip)
+                    .AppendLine("---")
+                    .AppendFormat("Gift wrap: {0}", shippingInfo.GiftWrap ? "Yes" : "No");
+
+                MailMessage mailMessage = new MailMessage(
+                    emailSettings.MailFromAddress,
+                    emailSettings.MailToAddress,
+                    "New order submitted",
+                    body.ToString());
+
+                if (emailSettings.WriteAsFile)
+                {
+                    mailMessage.BodyEncoding = Encoding.ASCII;
+                }
+
+                smtpClient.Send(mailMessage);
+            }
+        }
+    }
+}
