0438f2edae20518041d32464da5ac35947d04809
Philip.SeokJoon.Yun <seokjoon.yun@gmail.com>
2016-04-27 09:05:03 +0900

    fill added

---
+++ Pro.ASP.NET.MVC5/SportsStore/SportsStore.WebUI/Infrastructure/Binders/CartModelBinder.cs
@@ -0,0 +1,35 @@
+﻿using SportsStore.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace SportsStore.WebUI.Infrastructure.Binders
+{
+    public class CartModelBinder : IModelBinder
+    {
+        private const string sessionKey = "Cart";
+
+        public object BindModel(ControllerContext controllerContext, ModelBindingContext bindingContext)
+        {
+            Cart cart = null;
+            if (controllerContext.HttpContext.Server != null)
+            {
+                cart = (Cart)controllerContext.HttpContext.Session[sessionKey];
+            }
+
+            if (cart == null)
+            {
+                cart = new Cart();
+                if (controllerContext.HttpContext.Server != null)
+                {
+                    controllerContext.HttpContext.Session[sessionKey] = cart;
+                }
+            }
+
+            return cart;
+        }
+    }
+}
