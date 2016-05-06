9c0c4319c982bb54380e5f452ba8e8191a2cb17a
SeokJoon.Yun <seokjoon.yun@gmail.com>
2016-05-06 12:06:00 +0900

    ch11. Admin 인증 주석처리 (IIS7...)

--- Pro.ASP.NET.MVC5/SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
+++ Pro.ASP.NET.MVC5/SportsStore/SportsStore.WebUI/Controllers/AdminController.cs
@@ -5,7 +5,7 @@ using System.Web.Mvc;
 
 namespace SportsStore.WebUI.Controllers
 {
-    [Authorize]
+   //[Authorize]
     public class AdminController : Controller
     {
         private IProductRepository repository;
