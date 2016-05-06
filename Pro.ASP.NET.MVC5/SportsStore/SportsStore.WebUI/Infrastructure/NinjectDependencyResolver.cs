fb540e367758cf10f889c8003034d6f0783eb0b7
SeokJoon.Yun <seokjoon.yun@gmail.com>
2016-05-06 12:04:49 +0900

    ch12. Admin Authetication

--- Pro.ASP.NET.MVC5/SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
+++ Pro.ASP.NET.MVC5/SportsStore/SportsStore.WebUI/Infrastructure/NinjectDependencyResolver.cs
@@ -3,6 +3,8 @@ using Ninject;
 using SportsStore.Domain.Abstract;
 using SportsStore.Domain.Concrete;
 using SportsStore.Domain.Entities;
+using SportsStore.WebUI.Infrastructure.Abstract;
+using SportsStore.WebUI.Infrastructure.Concrete;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
@@ -43,6 +45,8 @@ namespace SportsStore.WebUI.Infrastructure
             };
 
             kernel.Bind<IOrderProcessor>().To<EmailOrderProcessor>().WithConstructorArgument("setting", emailSettings);
+
+            kernel.Bind<IAuthProvider>().To<FormsAuthProvider>();
         }
     }
 }
