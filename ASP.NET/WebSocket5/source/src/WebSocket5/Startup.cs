2381ee3e24799c079d0055311cd8c294e8a54f89
Philip.SeokJoon.Yun <seokjoon.yun@gmail.com>
2016-05-10 13:59:25 +0900

    ASP.NET5 WebSocket example

---
+++ ASP.NET/WebSocket5/source/src/WebSocket5/Startup.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Builder;
+using Microsoft.AspNet.Hosting;
+using Microsoft.AspNet.Identity.EntityFramework;
+using Microsoft.Data.Entity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using WebSocket5.Models;
+using WebSocket5.Services;
+using System.Net.WebSockets;
+using System.Collections.Concurrent;
+using System.Threading;
+using System.Text;
+
+namespace WebSocket5
+{
+    public class Startup
+    {
+        ConcurrentBag<WebSocket> _sockets = new ConcurrentBag<WebSocket>();
+
+        public Startup(IHostingEnvironment env)
+        {
+            // Set up configuration sources.
+
+            var builder = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true);
+
+            if (env.IsDevelopment())
+            {
+                // For more details on using the user secret store see http://go.microsoft.com/fwlink/?LinkID=532709
+                builder.AddUserSecrets();
+
+                // This will push telemetry data through Application Insights pipeline faster, allowing you to view results immediately.
+                builder.AddApplicationInsightsSettings(developerMode: true);
+            }
+
+            builder.AddEnvironmentVariables();
+            Configuration = builder.Build();
+        }
+
+        public IConfigurationRoot Configuration { get; set; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            // Add framework services.
+            services.AddApplicationInsightsTelemetry(Configuration);
+
+            services.AddEntityFramework()
+                .AddSqlServer()
+                .AddDbContext<ApplicationDbContext>(options =>
+                    options.UseSqlServer(Configuration["Data:DefaultConnection:ConnectionString"]));
+
+            services.AddIdentity<ApplicationUser, IdentityRole>()
+                .AddEntityFrameworkStores<ApplicationDbContext>()
+                .AddDefaultTokenProviders();
+
+            services.AddMvc();
+
+            // Add application services.
+            services.AddTransient<IEmailSender, AuthMessageSender>();
+            services.AddTransient<ISmsSender, AuthMessageSender>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddConsole(Configuration.GetSection("Logging"));
+            loggerFactory.AddDebug();
+
+            app.UseApplicationInsightsRequestTelemetry();
+
+            if (env.IsDevelopment())
+            {
+                app.UseBrowserLink();
+                app.UseDeveloperExceptionPage();
+                app.UseDatabaseErrorPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+
+                // For more details on creating database during deployment see http://go.microsoft.com/fwlink/?LinkID=615859
+                try
+                {
+                    using (var serviceScope = app.ApplicationServices.GetRequiredService<IServiceScopeFactory>()
+                        .CreateScope())
+                    {
+                        serviceScope.ServiceProvider.GetService<ApplicationDbContext>()
+                             .Database.Migrate();
+                    }
+                }
+                catch { }
+            }
+
+            app.UseIISPlatformHandler(options => options.AuthenticationDescriptions.Clear());
+
+            app.UseApplicationInsightsExceptionTelemetry();
+
+            app.UseStaticFiles();
+
+            app.UseIdentity();
+
+            // To configure external authentication please see http://go.microsoft.com/fwlink/?LinkID=532715
+
+            app.UseWebSockets();
+            app.Use(async (http, next) =>
+            {
+                if (http.WebSockets.IsWebSocketRequest)
+                {
+                    var webSocket = await http.WebSockets.AcceptWebSocketAsync();
+
+                    if (webSocket != null && webSocket.State == WebSocketState.Open)
+                    {
+                        //bool existed = false;
+                        //foreach (var socket in _sockets)
+                        //{
+                        //    if (socket == webSocket)
+                        //    {
+                        //        existed = true;
+                        //        break;
+                        //    }
+                        //}
+
+                        //if (!existed)
+                        //{
+                        //    _sockets.Add(webSocket);
+                        //}
+
+                        _sockets.Add(webSocket);
+
+                        // Handle the socket here
+                        while (webSocket.State == WebSocketState.Open)
+                        {
+                            var token = CancellationToken.None;
+                            var buffer = new ArraySegment<byte>(new byte[4096]);
+
+                            var received = await webSocket.ReceiveAsync(buffer, token);
+
+                            switch (received.MessageType)
+                            {
+                                case WebSocketMessageType.Text:
+                                    var request = Encoding.UTF8.GetString(buffer.Array, buffer.Offset, buffer.Count);
+                                    // Handle request here
+                                    //await webSocket.SendAsync(buffer, WebSocketMessageType.Text, true, token);
+                                    foreach (var socket in _sockets)
+                                    {
+                                        await socket.SendAsync(buffer, WebSocketMessageType.Text, true, token);
+                                    }
+                                    break;
+                            }
+                        }
+                    }
+                }
+                else
+                {
+                    await next();
+                }
+            });
+
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+
+        // Entry point for the application.
+        public static void Main(string[] args) => WebApplication.Run<Startup>(args);
+    }
+}
