b88f482f84e750ca7f669b741079fcbc68addbab
Philip.SeokJoon.Yun <seokjoon.yun@gmail.com>
2016-05-13 09:10:56 +0900

    ch18.filters.start

---
+++ Pro.ASP.NET.MVC5/Filters/Filters/Infrastructure/CustomAuthAttribute.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Filters.Infrastructure
+{
+    class CustomAuthAttribute : AuthorizeAttribute
+    {
+        private bool localAllowed;
+
+        public CustomAuthAttribute(bool allowed)
+        {
+            localAllowed = allowed;
+        }
+
+        protected override bool AuthorizeCore(HttpContextBase httpContext)
+        {
+            if (httpContext.Request.IsLocal)
+            {
+                return localAllowed;
+            }
+            else
+            {
+                return true;
+            }
+        }
+    }
+}
