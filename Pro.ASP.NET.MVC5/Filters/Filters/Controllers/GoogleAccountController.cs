1a4c81dab76b14fc83302e17d82c694808beeecc
Philip.SeokJoon.Yun <seokjoon.yun@gmail.com>
2016-05-16 07:47:45 +0900

    Filters

---
+++ Pro.ASP.NET.MVC5/Filters/Filters/Controllers/GoogleAccountController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+
+namespace Filters.Controllers
+{
+    public class GoogleAccountController : Controller
+    {
+        // GET: GoogleAccount
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(string username, string password, string returlUrl)
+        {
+            if (username.EndsWith("@google.com") && password == "secret")
+            {
+                FormsAuthentication.SetAuthCookie(username, false);
+                return Redirect(returlUrl ?? Url.Action("Index", "Home"));
+            }
+            else
+            {
+                ModelState.AddModelError("", "Incorrect username or password");
+                return View();
+            }
+        }
+    }
+}
--- Pro.ASP.NET.MVC5/Filters/Filters/Controllers/HomeController.cs
+++ Pro.ASP.NET.MVC5/Filters/Filters/Controllers/HomeController.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using Filters.Infrastructure;
 
 namespace Filters.Controllers
 {
@@ -14,6 +15,13 @@ namespace Filters.Controllers
         public string Index()
         {
             return "This is the Index Action on the Home controller";
+        }
+
+        [GoogleAuth]
+        [Authorize(Users = "bob@google.com")]
+        public string List()
+        {
+            return "This is the List action on the Home controller";
         }
     }
 }
