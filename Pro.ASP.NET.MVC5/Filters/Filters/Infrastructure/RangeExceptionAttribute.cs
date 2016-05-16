547e42d1bcba0e8115fda1afd9af0f594e2a8937
Philip.SeokJoon.Yun <seokjoon.yun@gmail.com>
2016-05-17 07:25:58 +0900

    ExceptionFilters

--- Pro.ASP.NET.MVC5/Filters/Filters/Infrastructure/RangeExceptionAttribute.cs
+++ Pro.ASP.NET.MVC5/Filters/Filters/Infrastructure/RangeExceptionAttribute.cs
@@ -13,7 +13,12 @@ namespace Filters.Infrastructure
         {
             if (!filterContext.ExceptionHandled && filterContext.Exception is ArgumentOutOfRangeException)
             {
-                filterContext.Result = new RedirectResult("~/Content/RangeErrorPage.html");
+                int val = (int)(((ArgumentOutOfRangeException)filterContext.Exception).ActualValue);
+                filterContext.Result = new ViewResult
+                {
+                    ViewName = "RangeError",
+                    ViewData = new ViewDataDictionary<int>(val)
+                };
                 filterContext.ExceptionHandled = true;
             }
         }
