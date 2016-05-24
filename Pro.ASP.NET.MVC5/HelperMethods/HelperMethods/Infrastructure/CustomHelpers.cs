8f7ca0d140d686fb709f24991cb39517bca9f554
Philip.SeokJoon.Yun <seokjoon.yun@gmail.com>
2016-05-25 08:50:52 +0900

    HelperMethod

---
+++ Pro.ASP.NET.MVC5/HelperMethods/HelperMethods/Infrastructure/CustomHelpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace HelperMethods.Infrastructure
+{
+    public static class CustomHelpers
+    {
+        public static MvcHtmlString ListArrayItems(this HtmlHelper html, string[] list)
+        {
+            TagBuilder tag = new TagBuilder("ul");
+
+            foreach(string str in list)
+            {
+                TagBuilder itemTag = new TagBuilder("li");
+                itemTag.SetInnerText(str);
+                tag.InnerHtml += itemTag.ToString();
+            }
+
+            return new MvcHtmlString(tag.ToString());
+        }
+    }
+}
