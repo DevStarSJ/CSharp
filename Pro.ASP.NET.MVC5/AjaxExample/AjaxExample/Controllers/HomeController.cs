2cce7c9c2d8ca09625a44fa0c4237f1a577eace4
Philip.SeokJoon.Yun <seokjoon.yun@gmail.com>
2016-05-26 08:59:32 +0900

    AjaxExample : Ajax을 사용안한 Page

---
+++ Pro.ASP.NET.MVC5/AjaxExample/AjaxExample/Controllers/HomeController.cs
@@ -0,0 +1,38 @@
+﻿using AjaxExample.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AjaxExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        private Person[] personData = {
+            new Person { FirstName = "Adam", LastName = "Freeman", Role = Role.Admin },
+            new Person { FirstName = "Jacqui", LastName = "Griffyth", Role = Role.User },
+            new Person { FirstName = "John", LastName = "Smith", Role = Role.User },
+            new Person { FirstName = "Anne", LastName = "Jones", Role = Role.Guest }
+        };
+
+        public ActionResult Index()
+        {
+            return View(personData);
+        }
+
+        [HttpPost]
+        public ActionResult Index(string selectedRole)
+        {
+            if (selectedRole == null || selectedRole == "All")
+            {
+                return View(personData);
+            }
+            else
+            {
+                Role seleted = (Role)Enum.Parse(typeof(Role), selectedRole);
+                return View(personData.Where(p => p.Role == seleted));
+            }
+        }
+    }
+}
