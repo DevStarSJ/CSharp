bb3157bfa9d5332d3debd1c6d5dc15850764a5a9
Philip.SeokJoon.Yun <seokjoon.yun@gmail.com>
2016-06-07 08:42:51 +0900

    Hello Akka

---
+++ Akka/0.1GettingStart/ex01/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Akka;
+using Akka.Actor;
+
+namespace ex01
+{
+    public class Greet
+    {
+        public string Who { get; private set; }
+
+        public Greet(string who)
+        {
+            Who = who;
+        }
+    }
+
+    public class GreetingActor : ReceiveActor
+    {
+        public GreetingActor()
+        {
+            Receive<Greet>(g => Console.WriteLine($"Hello {g.Who}"));
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var system = ActorSystem.Create("MySystem");
+
+            var greeter = system.ActorOf<GreetingActor>("greeter");
+            greeter.Tell(new Greet("World"));
+
+            Console.ReadLine();
+        }
+    }
+}
